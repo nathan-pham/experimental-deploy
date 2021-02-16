5ab5b25afce06eab7d24e0bdabf46a43403e4e8e
Nathan Pham <nathanpham.me@gmail.com>
2021-02-15 17:38:30 -0800

    setting styles

--- assets/components/editor/header.js
+++ assets/components/editor/header.js
@@ -13,6 +13,28 @@ const Header = ({ id, name, description }) => {
 
     const toggle = () => setVisible(state => !state) 
 
+    const addDependency = (e) => {
+        if(e.key.toLowerCase() == "enter") {
+            const dependency = e.target.value
+            e.target.value = ""
+            
+            setSettings(state => ({
+                ...state,
+                dependencies: [
+                    ...state.dependencies,
+                    dependency
+                ]
+            }))
+        }
+    }
+
+    const removeDependency = (index) => {
+        setSettings(state => ({
+            ...state,
+            dependencies: state.dependencies.filter(v => v !== state.dependencies[index])
+        }))
+    }
+
     return (
         <>
             <header className="editor-header flex justify-between align-center">
@@ -37,13 +59,23 @@ const Header = ({ id, name, description }) => {
             { visible ? (
                 <Modal onClick={toggle}>
                     <h1>Settings</h1>
-                    <input />
-                    <div className="dependencies"></div>
+                    <div className="flex justify-between align-center" style={{margin: "0 0 1rem 0"}}>
+                        <button className="primary-button"><i className="fas fa-link"></i>Open preview</button>
+                        <button className="secondary-button"><i className="fas fa-code-branch"></i>Fork</button>
+                    </div>
+                    <input placeholder="Enter a dependency" type="text" style={{margin: "0 0 1rem 0"}} onKeyDown={addDependency}></input>
+                    <div className="dependencies">
+                        { 
+                            settings.dependencies.reverse().filter(v => v.length).map((v, i) => (
+                                <div className="flex align-center"><i key={i} className="fas fa-minus-circle" onClick={() => removeDependency(i)}></i> {v}</div>
+                            )) 
+                        }
+                    </div>
                     <div className="flex justify-between align-center">
                         <span className="label">Anyone can see this experiment</span>
-                        <ToggleSwitch />
+                        <ToggleSwitch onChange={() => console.log('changed')} />
                     </div>
-                    <button className="delete">Delete project</button>
+                    <button className="danger-button" style={{margin: "1rem 0 0 0"}}><i className="fas fa-trash-alt"></i> Delete project</button>
                 </Modal>
             ) : null }
         </>
