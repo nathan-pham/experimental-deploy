c6978fdffc35a8a2ebb9188962ac518ce2650207
Nathan Pham <nathanpham.me@gmail.com>
2021-02-16 19:23:40 -0800

    experiment component

---
+++ assets/components/experiment.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+const Experiment = ({ href }) => {
+    return (
+        <article className="experiment">
+            
+        </article>
+    )
+}
+
+export default Experiment
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../assets/styles/globals.css"
 import "../assets/styles/editor.css"
 import "../assets/styles/error.css"
+import "../assets/styles/index.css"
 import "../assets/styles/flex.css"
 
 const App = ({ Component, pageProps }) => {
