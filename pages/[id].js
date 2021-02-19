fd53f455cea387d65cc1fd4a06683939eb8d68b0
Nathan Pham <nathanpham.me@gmail.com>
2021-02-19 17:48:03 +0000

    replit preview

--- pages/[id].js
+++ pages/[id].js
@@ -2,6 +2,8 @@ import { collections } from "../assets/database/client"
 import Root from "../assets/components/document/root"
 import Error from "../assets/components/error"
 import { generateCode } from "../assets/utils"
+import * as jsdom from "jsdom"
+const { JSDOM } = jsdom
 
 const ProjectEditor = ({ id, fetchedProject }) => {
     return (
@@ -14,7 +16,11 @@ const ProjectEditor = ({ id, fetchedProject }) => {
             : (
                 <Root meta={fetchedProject.meta}>
                     <div className="iframe-wrapper">
-                        <iframe srcDoc={generateCode(fetchedProject)}></iframe>
+                        {
+                            fetchedProject.repl
+                                ? <iframe src={fetchedProject.repl}></iframe>
+                                : <iframe srcDoc={generateCode(fetchedProject)}></iframe>
+                        }
                     </div>
                 </Root>
             )
@@ -26,7 +32,25 @@ const getServerSideProps = async ({ params }) => {
     const { id } = params
 
     const document = await collections.projects.doc(id).get()
-    const project = document.exists ? document.data() : { error: "project not found" }
+    let project = document.exists ? document.data() : { error: "project not found" }
+    
+    if(!document.exists) {
+        const repl = `https://${Buffer.from(id, "base64").toString("binary")}.phamn23.repl.co/`
+        const res = await fetch(repl).catch(e => ({ status: 404 }))
+
+        if(res.status == 200) {
+            const body = await res.text()
+            const replDocument = new JSDOM(body).window.document
+            project = {
+                repl,
+                name: replDocument.querySelector("title").textContent,
+                description: replDocument.querySelector("meta[name='description']").getAttribute("content"),
+            }
+        }
+        else {
+            project = { error: "project not found" }
+        }
+    }
 
     return ({
         props: {
